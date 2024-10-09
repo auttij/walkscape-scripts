3a59132c0d243ed60edfa043776936295eecc81a
auttij <juhana.autti@gmail.com>
2024-10-09 10:11:31 +0300

    fix diff generation

--- diff_tool.py
+++ diff_tool.py
@@ -1,73 +1,96 @@
-from file_utils import read_json, json_files, get_localization
+from file_utils import read_json, write_json, json_files, get_localization
 from jsondiff import diff
 import filecmp
+from os.path import exists
 
 class DiffTool:
-  def __init__(self, src_1, src_2, excluded_keys=None) -> None:
-    self.src_1 = src_1
-    self.src_2 = src_2
+  def __init__(self, included_keys=None, excluded_keys=None) -> None:
+    self.included = included_keys if  included_keys else []
     self.excluded = excluded_keys if excluded_keys else []
     self.diff = {}
-    self.create_diff_structure()
-    self.generate_diffs()
 
-  def create_diff_structure(self):
+  def create_diff_structure(self, src_1, src_2):
     def trim_filename(filename):
       return filename.split('/')[-1].split('\\')[-1].split('.')[0]
     
-    relatative_files_1 = json_files(self.src_1, True)
-    relatative_files_2 = json_files(self.src_2, True)
+    relatative_files_1 = json_files(src_1, True)
+    relatative_files_2 = json_files(src_2, True)
     in_both = [trim_filename(i) for i in relatative_files_1 if i in relatative_files_2]
 
-    absolute_files_1 = json_files(self.src_1)
-    absolute_files_2 = json_files(self.src_2)
+    absolute_files_1 = json_files(src_1)
+    absolute_files_2 = json_files(src_2)
 
-    all = {
-      e: { 'path_1': absolute_files_1[i], 'path_2': absolute_files_2[i] } for i, e in enumerate(in_both)
+    def list_find(l, subs):
+      return [i for i in l if subs in i][0]
+
+    return {
+      e: { 'path_1': list_find(absolute_files_1, e), 'path_2': list_find(absolute_files_2, e) } for e in in_both
     }
-    not_excluded = { k: v for k, v in all.items() if k not in self.excluded }
-    self.diff = not_excluded
 
-  def file_diff(self, key):
+  def base_diff_from_src(self, src_1, src_2, filename=None):
+    diff_strucutre = self.create_diff_structure(src_1, src_2)
+    base_diff = self.generate_diffs(diff_strucutre)
+    if filename:
+      write_json(filename, base_diff)
+    return base_diff
+
+  def base_diff_from_file(self, filename):
+    return read_json(filename)
+
+  def create_diff(self, src_1, src_2, filename=None):
+    if filename and exists(filename):
+      print(f'loading diff from file: {filename}')
+      base_diff = self.base_diff_from_file(filename)
+    else:
+      print('generating diff from source')
+      base_diff = self.base_diff_from_src(src_1, src_2, filename)
+
+  def generate_diffs(self, base_diff):
+    for key, data in base_diff.items():
+      base_diff[key]['diff'] = self.file_diff(key, data)
+    print(base_diff)
+    return base_diff
+
+  def file_diff(self, key, obj):
     print(f'generating diff for: {key}')
-    obj = self.diff[key]
     path_1 = obj['path_1']
     path_2 = obj['path_2']
 
     if filecmp.cmp(path_1, path_2, shallow=False):
-      self.diff[key]['diff'] = {}
-      return
+      print('no changes in file')
+      return {}
 
     data_1 = read_json(path_1)
     data_2 = read_json(path_2)
 
-    base_diff = diff(data_1, data_2, syntax='symmetric')
-
-    flat_diff = { k: v for k, v in base_diff.items() if str(k).isdigit() }
-    for k in flat_diff:
-      flat_diff[k]['action'] = 'update'
-
-    others = [k for k in base_diff.keys() if not str(k).isdigit()]
-    for diff_key in others:
-      new_dict = { k: v for k, v in base_diff[diff_key] }
-      for k in new_dict:
-        new_dict[k]['action'] = str(diff_key)
-      flat_diff = flat_diff | new_dict
+    base_diff = diff(data_1, data_2, syntax='symmetric', marshal=True)
+    return base_diff
 
 
-    def name(key):
-      try:
-        return get_localization(key)
-      except:
-        return key
+  # def populate_diff(self):
+  #   flat_diff = { k: v for k, v in base_diff.items() if str(k).isdigit() }
+  #   for k in flat_diff:
+  #     flat_diff[k]['action'] = 'update'
 
-    named_diff = { name(data_2[k]['name']): v for k, v in flat_diff.items() }
+  #   others = [k for k in base_diff.keys() if not str(k).isdigit()]
+  #   for diff_key in others:
+  #     new_dict = { k: v for k, v in base_diff[diff_key] }
+  #     for k in new_dict:
+  #       new_dict[k]['action'] = str(diff_key)
+  #     flat_diff = flat_diff | new_dict
 
-    self.diff[key]['diff'] = named_diff
 
-  def generate_diffs(self):
-    for key in self.diff:
-      self.file_diff(key)
+  #   def name(key):
+  #     try:
+  #       return get_localization(key)
+  #     except:
+  #       return key
+
+  #   named_diff = { name(data_2[k]['name']): v for k, v in flat_diff.items() }
+
+  #   self.diff[key]['diff'] = named_diff
+
+
 
   def pretty_print(self):
     print()
@@ -158,14 +181,15 @@ class DiffTool:
       print(key, value)
 
 def main():
-  path_0 = "C:\\Games\\Walkscape\\0.2.0-beta+322 data"
-  path_1 = "C:\\Games\\Walkscape\\0.2.0-beta+323\\data\\flutter_assets\\assets\\data"
-  path_2 = "C:\\Games\\Walkscape\\0.2.0-beta+324\\data\\flutter_assets\\assets\\data"
+  path_0 = "C:\\Games\\Walkscape\\data files\\0.2.0-beta+322"
+  path_1 = "C:\\Games\\Walkscape\\data files\\0.2.0-beta+323"
+  path_2 = "C:\\Games\\Walkscape\\data files\\0.2.0-beta+324"
   excluded = [] #['parallaxes', 'pfp_options', 'items', 'achievements', 'activities', 'locations', 'attributes', 'buildings', 'characters', 'default_unlocks', 'factions', 'game_data', 'items', 'job_boards', 'banned_keywords', 'keywords', 'loot_tables', 'pfp_option_groups', 'reward_progress', 'routes', 'services', 'shops', 'stats', 'terrain_modifiers']
 
 
-  d = DiffTool(path_0, path_1, excluded_keys=excluded)
-  d.pretty_print()
+  d = DiffTool()
+  d.base_diff_from_src(path_0, path_1, "323_diff.json")
+  # d.pretty_print()
 
 if __name__ == '__main__':
   main()
